90c350f72f49e5046fc174a6289e234167094d85
codingbac <125853535+codingbac@users.noreply.github.com>
2024-03-14 19:09:55 +0545

    Add files via upload

---
+++ E-learning Management System/index.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- CSS bootstrap -->
+    <link rel="stylesheet" href="CSS/bootstrap.min.css">
+
+    <!-- font awesome css -->
+    <link rel="stylesheet" href="CSS/all.min.css">
+
+    <!-- Google Font -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@700&display=swap" rel="stylesheet">
+
+    <!-- custom css -->
+    <link rel="stylesheet" href="CSS/style.css">
+
+    <title>Le-Earn</title>
+
+</head>
+<body>
+
+<!-- Navigation Bar -->
+<nav class="navbar navbar-expand-sm navbar-dark bg-dark px-5 fixed-top">
+  <!-- px-5 is bootstrap compatible -->
+  <!-- fixed top = fixed video -->
+  <div class="container-fluid">
+    <i><a class="navbar-brand" href="index.php"><i>Le-Earn</i></a></i>
+    <span class="navbar-text"><b>Learn to remove L</b></span>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <ul class="navbar-nav custom-nav px-5"> 
+        <li class="nav-item custom-nav-item"><a href="index.php" class="nav-link">Home</a></li>
+        <li class="nav-item custom-nav-item"><a href="#" class="nav-link">Courses</a></li>
+        <li class="nav-item custom-nav-item"><a href="#" class="nav-link">Payment Status</a></li>
+        <li class="nav-item custom-nav-item"><a href="#" class="nav-link">My Profile</a></li>
+        <li class="nav-item custom-nav-item"><a href="#" class="nav-link">Login</a></li>
+        <li class="nav-item custom-nav-item"><a href="#" class="nav-link">Logout</a></li>
+        <li class="nav-item custom-nav-item"><a href="#" class="nav-link">Sign Up</a></li>
+        <li class="nav-item custom-nav-item"><a href="#" class="nav-link">Feedback</a></li>
+        <li class="nav-item custom-nav-item"><a href="#" class="nav-link">Contact</a></li>
+    </ul>
+    </div>
+  </div>
+</nav>
+
+<!-- End Nav Bar -->
+
+
+<!-- Start Video Background -->
+<div class="container-fluid remove-vid-marg">
+  <div class="vid-parent">
+    <video playsinline autoplay muted loop>
+      <source src="Video/students_-_63328 (Original).mp4">
+    </video>
+    <div class="vid-overlay"></div>
+  </div>
+  <div class="vid-content">
+    <u><h1>Start to learn in <i>Le-Earn</i></h1></u>
+    <small class="my-content"><u><b>"Learn to Remove L"</b></u></small><br>
+    <br><a href="#" class="btn btn-danger">Get Started</a>
+  </div>
+</div>
+<!-- End Background -->
+
+
+<!-- Start Text Banner -->
+<div class="container-fluid bg-danger txt-banner">
+<div class="row bottom-banner">
+<div class="col-sm">
+  <h5><i class="fa-solid fa-book-open"></i>100+ Online Courses</h5>
+</div>
+<div class="col-sm">
+  <h5><i class="fa-solid fa-user"></i>Expert Instructors</h5>
+</div>
+<div class="col-sm">
+  <h5><i class="fa-solid fa-heart-pulse"></i>Lifetime Access</h5>
+</div>
+<div class="col-sm">
+  <h5><i class="fa-solid fa-money-bill-transfer"></i>Money Back Guarantee</h5>
+</div>
+
+  </div>
+    </div>
+
+
+<!-- End Text Banner -->
+
+
+
+
+
+
+<!-- JS bootstrap -->
+<script src="js/bootstrap.min.js"></script>
+<!-- font awesome js -->
+<script src="js/all.min.js"></script>
+
+
+    
+</body>
+</html>
